b9527c51a214401718d3edec5b854af481b273fa
Randal Chacon <randalchacon2413@gmail.com>
2022-06-01 23:20:10 -0300

    SIMULADOR INTERACTIVO

--- js/index.js
+++ js/index.js
@@ -1,17 +1,63 @@
-let sumarNumero = parseInt(prompt("Ingresar numero para sumar"))
+function compras(){
+    let prenda = prompt("Ingrese la prenda que desea comprar(BUZO O JEAN)").toLowerCase()
+     if(prenda == "buzo"){
 
-for (let index =0; index <=10 ; index++) {
-    let suma = sumarNumero + index
-    alert(sumarNumero + "+" + index +"="+ suma)
+        let talle = parseInt (prompt("ingrese el talle"));  //no lo toma, no se porque..//
 
-}
+        let talles =  prompt("ingrese el talle");
+        let unidad = parseInt (prompt("Ingrese las unidades"));
+        let precioBuzo = 4600
 
-let restarNumero = parseInt(prompt("Ingresar numero para restar"))
+        const suma = (a,b) => { return a + b }
+        const resta = (a,b) => { return a - b}
+        const multiplicacion = (a,b) => a*b 
+
+        let subTotal = multiplicacion(precioBuzo, unidad)
+        const iva = subTotal*0.21
+        const descuento = subTotal*0.35
+
+       let precioFinal = resta(suma(subTotal, iva), descuento)
 
 
-for (let index = 0; index <=10 ; index++) {
-    let resta = index - restarNumero
-    alert(restarNumero + "-" + index +"="+ resta)
 
-}
 
+        let compraFinal = alert(`Tu prenda es ${prenda} con la cantidad de ${unidad} unidades y talle ${talles} con un precio final de ${precioFinal} `)
+
+        if(precioFinal > 18000){
+            alert("ENVIO GRATIS")
+        }else{
+            alert("ENVIO $500")
+        }
+
+
+     } else if (prenda == "jean") {
+        let talle = parseInt (prompt("ingrese el talle"));  //no lo toma, no se porque..//
+
+        let talles =  prompt("ingrese el talle");
+        let unidad = parseInt (prompt("Ingrese las unidades"));
+        let precioJean = 5500
+
+        const suma = (a,b) => { return a + b }
+        const resta = (a,b) => { return a - b}
+        const multiplicacion = (a,b) => a*b 
+
+        let subTotal = multiplicacion(precioJean, unidad)
+        const iva = subTotal*0.21
+        const descuento = subTotal*0.35
+
+       let precioFinal = resta(suma(subTotal, iva), descuento)
+
+
+        let compraFinal = alert(`Tu prenda es ${prenda} con la cantidad de ${unidad} unidades y talle ${talles} con un precio final de ${precioFinal} `)
+
+        if(precioFinal > 20000){
+            alert("ENVIO GRATIS")
+        }else{
+            alert("ENVIO $500")
+        }
+
+
+     }
+
+    }
+    compras() 
